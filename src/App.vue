71c8a683b7373834683da007a4213df8c51d389e
Md. Ruhul Amin Khan <39692985+ruhulite@users.noreply.github.com>
2020-05-19 17:48:11 +0600

    environment setup

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,53 @@
 <template>
-    <ag-grid-vue style="width: 500px; height: 500px;"
-                 class="ag-theme-alpine"
-                 :columnDefs="columnDefs"
-                 :rowData="rowData">
+  <div>
+    <ag-grid-vue
+      style="width: 800px; height: 500px;"
+      class="ag-theme-alpine"
+      :columnDefs="columnDefs"
+      :rowData="rowData"
+      rowSelection="multiple"
+    >
     </ag-grid-vue>
+  </div>
 </template>
 
 <script>
-    import {AgGridVue} from "ag-grid-vue";
+import {AgGridVue} from 'ag-grid-vue';
 
-    export default {
-        name: 'App',
-        data() {
-            return {
-                columnDefs: null,
-                rowData: null
-            }
-        },
-        components: {
-            AgGridVue
-        },
-        beforeMount() {
-            this.columnDefs = [
-                {headerName: 'Make', field: 'make'},
-                {headerName: 'Model', field: 'model'},
-                {headerName: 'Price', field: 'price'}
-            ];
+export default {
+  name: 'App',
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+    };
+  },
+  components: {
+    AgGridVue,
+  },
+  beforeMount() {
+    this.columnDefs = [
+      {
+        headerName: 'Make',
+        field: 'make',
+        sortable: true,
+        filter: true,
+        checkboxSelection: true,
+      },
+      {headerName: 'Model', field: 'model', sortable: true, filter: true},
+      {headerName: 'Price', field: 'price', sortable: true, filter: true},
+    ];
 
-            this.rowData = [
-                {make: 'Toyota', model: 'Celica', price: 35000},
-                {make: 'Ford', model: 'Mondeo', price: 32000},
-                {make: 'Porsche', model: 'Boxter', price: 72000}
-            ];
-        }
-    }
+    this.rowData = [
+      {make: 'Toyota', model: 'Celica', price: 35000},
+      {make: 'Ford', model: 'Mondeo', price: 32000},
+      {make: 'Porsche', model: 'Boxter', price: 72000},
+    ];
+  },
+};
 </script>
 
 <style lang="scss">
-  @import '../node_modules/ag-grid-community/dist/styles/ag-grid.css';
-  @import '../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css';
+@import '../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+@import '../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css';
 </style>
